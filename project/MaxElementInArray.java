c0d62bf0a7ac6b9f82e18a0134c418023991cfb7
Alex <XRaid555@gmail.com>
2026-01-01 15:08:56 +0500

    Внесли изменения

--- project/MaxElementInArray.java
+++ project/MaxElementInArray.java
@@ -1,9 +1,9 @@
 package project;
 
-public class MinElementInArray {
+public class MaxElementInArray {
     public static void main(String[] args) {
         int[] array = {5, 3, 9, 1, 7, 2, 8, 4, 6};
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
 
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
